9a3b92af8f13e09712d6cfbf57b2ebef3eded0a0
Jacobtread <jacobtread@gmail.com>
2025-05-20 22:11:16 +1200

    refactor: port event log to plain sql

--- src-tauri/src/database/entity/event_log.rs
+++ src-tauri/src/database/entity/event_log.rs
@@ -1,28 +1,10 @@
+use super::shared::{LoggingLevelDb, LogsQuery};
+use crate::database::{DbPool, DbResult};
 use chrono::{DateTime, Utc};
-use sea_query::{Expr, Func, IdenStatic, Order, Query};
+use itertools::Itertools;
 use serde::{Deserialize, Serialize};
 use sqlx::prelude::FromRow;
 use uuid::Uuid;
-
-use crate::database::{
-    helpers::{sql_exec, sql_query_all, sql_query_one_single},
-    DbPool, DbResult,
-};
-
-use super::shared::{LoggingLevelDb, LogsQuery};
-
-#[derive(IdenStatic, Copy, Clone)]
-#[iden(rename = "event_logs")]
-pub struct EventLogsTable;
-
-#[derive(IdenStatic, Copy, Clone)]
-pub enum EventLogsColumn {
-    Id,
-    EventId,
-    Level,
-    Message,
-    CreatedAt,
-}
 
 #[derive(Debug, Clone, Serialize, Deserialize, FromRow)]
 pub struct EventLogsModel {
@@ -51,25 +33,16 @@ impl EventLogsModel {
     pub async fn create(db: &DbPool, create: CreateEventLog) -> DbResult<()> {
         let id = Uuid::new_v4();
 
-        sql_exec(
-            db,
-            Query::insert()
-                .into_table(EventLogsTable)
-                .columns([
-                    EventLogsColumn::Id,
-                    EventLogsColumn::EventId,
-                    EventLogsColumn::Level,
-                    EventLogsColumn::Message,
-                    EventLogsColumn::CreatedAt,
-                ])
-                .values_panic([
-                    id.into(),
-                    create.event_id.into(),
-                    create.level.into(),
-                    create.message.into(),
-                    create.created_at.into(),
-                ]),
+        sqlx::query(
+            r#"INSERT INTO "event_logs" ("id", "event_id", "level", "message", "created_at")
+            VALUES (?, ?, ?, ?, ?)"#,
         )
+        .bind(id)
+        .bind(create.event_id)
+        .bind(create.level)
+        .bind(create.message)
+        .bind(create.created_at)
+        .execute(db)
         .await?;
 
         Ok(())
@@ -79,77 +52,87 @@ impl EventLogsModel {
     pub async fn query(
         db: &DbPool,
         event_id: Uuid,
-        query: LogsQuery,
+        input: LogsQuery,
     ) -> DbResult<Vec<EventLogsModel>> {
-        let mut select = Query::select();
-        select
-            .from(EventLogsTable)
-            .columns([
-                EventLogsColumn::Id,
-                EventLogsColumn::EventId,
-                EventLogsColumn::Level,
-                EventLogsColumn::Message,
-                EventLogsColumn::CreatedAt,
-            ])
-            .and_where(Expr::col(EventLogsColumn::EventId).eq(event_id))
-            .and_where_option(
-                query
-                    .level
-                    .map(|level| Expr::col(EventLogsColumn::Level).eq(level as i32)),
-            )
-            .and_where_option(
-                query
-                    .start_date
-                    .map(|start_date| Expr::col(EventLogsColumn::CreatedAt).gt(start_date)),
-            )
-            .and_where_option(
-                query
-                    .end_date
-                    .map(|end_date| Expr::col(EventLogsColumn::CreatedAt).lt(end_date)),
-            )
-            .order_by(EventLogsColumn::CreatedAt, Order::Desc);
+        let condition = std::iter::once(r#""event_id" = ?"#)
+            // Optional level filter
+            .chain(input.level.map(|_| r#"WHERE "level" = ?"#))
+            // Filter from start date
+            .chain(input.start_date.map(|_| r#"WHERE "created_at" >= ?"#))
+            // Filter from end date
+            .chain(input.end_date.map(|_| r#"WHERE "created_at" <= ?"#))
+            // Join into condition
+            .join(" OR ");
 
-        if let (Some(offset), Some(limit)) = (query.offset, query.limit) {
-            select.offset(offset).limit(limit);
+        let offset = if input.offset.is_some() && input.limit.is_some() {
+            "OFFSET ? LIMIT ?"
+        } else {
+            ""
+        };
+
+        let sql = format!(
+            r#"SELECT * FROM "event_logs" WHERE {condition} {offset} 
+            ORDER BY "created_at" DESC"#
+        );
+
+        let mut query = sqlx::query_as(&sql)
+            // Bind event ID
+            .bind(event_id);
+
+        if let Some(level) = input.level {
+            query = query.bind(level as i32)
         }
 
-        sql_query_all(db, &select).await
+        if let Some(start_date) = input.start_date {
+            query = query.bind(start_date)
+        }
+
+        if let Some(end_date) = input.end_date {
+            query = query.bind(end_date)
+        }
+
+        if let (Some(offset), Some(limit)) = (input.offset, input.limit) {
+            query = query.bind(offset as i64).bind(limit as i64)
+        }
+
+        query.fetch_all(db).await
     }
 
     /// Deletes all logs that happened before the provided `start_time`.
     /// Used to clean out old logs
     pub async fn delete_before(db: &DbPool, start_date: DateTime<Utc>) -> DbResult<()> {
-        sql_exec(
-            db,
-            Query::delete()
-                .from_table(EventLogsTable)
-                .and_where(Expr::col(EventLogsColumn::CreatedAt).lt(start_date)),
-        )
-        .await
+        sqlx::query(r#"DELETE FROM "event_logs" WHERE "created_at" < ?"#)
+            .bind(start_date)
+            .execute(db)
+            .await?;
+
+        Ok(())
     }
 
     /// Deletes a collection of specific logs by ID
     pub async fn delete_by_ids(db: &DbPool, ids: &[Uuid]) -> DbResult<()> {
-        sql_exec(
-            db,
-            Query::delete()
-                .from_table(EventLogsTable)
-                .and_where(Expr::col(EventLogsColumn::Id).is_in(ids.iter().copied())),
-        )
-        .await
+        if ids.is_empty() {
+            return Ok(());
+        }
+
+        let placeholders = std::iter::repeat('?').take(ids.len()).join(",");
+        let sql = format!(r#"DELETE FROM "event_logs" WHERE "id" IS IN ({placeholders})"#);
+        let mut query = sqlx::query(&sql);
+
+        for id in ids {
+            query = query.bind(id);
+        }
+
+        query.execute(db).await?;
+        Ok(())
     }
 
     /// Estimate the size of all log messages in the database
     pub async fn estimated_size(db: &DbPool) -> DbResult<u32> {
-        sql_query_one_single(
-            db,
-            Query::select().from(EventLogsTable).expr(Func::coalesce([
-                // Get total length of all message text
-                Func::sum(Func::char_length(Expr::col(EventLogsColumn::Message))).into(),
-                // Fallback to zero
-                Expr::value(0),
-            ])),
-        )
-        .await
+        let result: (u32,) =
+            sqlx::query_as(r#"SELECT COALESCE(SUM(LENGTH("message")), 0) FROM "event_logs""#)
+                .fetch_one(db)
+                .await?;
+        Ok(result.0)
     }
 }
